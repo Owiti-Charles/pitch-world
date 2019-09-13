ccc026a9a8f1683c317f7048ec81f9d80e5ba457
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-13 15:38:13 +0300

    Create config file

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+from config import Config
+
+app = Flask(__name__)
+
+def create_app():
+    app.config.from_object(Config)
+    from .auth import auth as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ manage.py
@@ -0,0 +1,10 @@
+from flask_script import Manager, Server
+from app import create_app
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+if __name__ == "__main__":
+    manager.run
