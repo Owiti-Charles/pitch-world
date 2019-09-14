76aaae2c0c8b73bc2dbdac66c6e6f2c5cc99723b
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-14 09:58:29 +0300

    Add more tests to tests class for User model

--- manage.py
+++ manage.py
@@ -12,7 +12,13 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
     manager.run()
--- tests/user_test.py
+++ tests/user_test.py
@@ -8,3 +8,10 @@ class UserTest(unittest.TestCase):
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.password is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('qwerty'))
