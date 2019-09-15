c44111df94392a5ae458494e10f6e393cf99629f
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-15 18:46:30 +0300

    Categorize all posts based on their categories

--- app/main/views.py
+++ app/main/views.py
@@ -8,8 +8,10 @@ from .. import db,photos
 @main.route('/')
 def index():
     pitches = Pitch.query.all()
-    job = Pitch.query.filter_by(category = 'job')   
-    return render_template('index.html', job = job, pitches=pitches)
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
 
 @main.route('/new_pitch', methods = ['POST','GET'])
 @login_required
@@ -25,19 +27,20 @@ def new_pitch():
         return redirect(url_for('main.index'))
     return render_template('create_pitch.html', form = form)
 
-@main.route('/new_comment/<int:pitch_id>', methods = ['POST','GET'])
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
 @login_required
 def comment(pitch_id):
     form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
     if form.validate_on_submit():
-        comment = form.comment.data
-        post_id = Pitch.query.get(pitch_id)
+        comment = form.comment.data 
+        pitch_id = pitch_id
         user_id = current_user._get_current_object().id
         new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
-
         new_comment.save_c()
-        return redirect(url_for('.comment', pitch_id = pitch_id))
-    return render_template('comment.html', form =form,)
+        return redirect(url_for('.comment', pitch = pitch))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
 
 
 @main.route('/user/<name>')
