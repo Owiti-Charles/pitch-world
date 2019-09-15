5d0b18ea91da275ac580a2a7eb9a761426a7469a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-15 09:30:16 +0300

    Create piches model class to define each individual pitch

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db,login_manager
+from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 
@@ -10,6 +11,7 @@ class User(db.Model,UserMixin):
     secure_password = db.Column(db.String(255),nullable = False)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    pitches = db.relationship('Pitch', backref='users', lazy='dynamic')
     
 
     @property
@@ -23,7 +25,6 @@ class User(db.Model,UserMixin):
     def verify_password(self, password):
         return check_password_hash(self.secure_password,password) 
     
-    
     def save_u(self):
         db.session.add(self)
         db.session.commit()
@@ -32,9 +33,21 @@ class User(db.Model,UserMixin):
         db.session.delete(self)
         db.session.commit()
     
-    
     def __repr__(self):
         return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    post = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True)
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
