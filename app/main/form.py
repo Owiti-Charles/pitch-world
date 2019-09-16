c720c2624409a0d3c2abcc601b29899e439b46c9
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-16 12:44:13 +0300

    Style the home page to be more appealing

--- app/main/form.py
+++ app/main/form.py
@@ -10,7 +10,7 @@ class PitchForm(FlaskForm):
     title = StringField('Title', validators=[Required()])
     post = TextAreaField('Your Pitch', validators=[Required()])
     category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
-    submit = SubmitField('Post')
+    submit = SubmitField('Pitch')
 
 class CommentForm(FlaskForm):
     comment = StringField('Leave a comment',validators=[Required()])
