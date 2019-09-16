33bd3e06938013051b28b3ae504e5e345185902a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-16 22:43:04 +0300

    Create email.py under app to define how the email is sent

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask 
 from config import Config
+from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 
 db = SQLAlchemy()
+mail = Mail()
 bootstap = Bootstrap()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -27,6 +29,7 @@ def create_app():
     db.init_app(app)
     bootstap.init_app(app)
     configure_uploads(app,photos)
+    mail.init_app(app)
     
 
 
--- config.py
+++ config.py
@@ -7,4 +7,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     Debug=True
